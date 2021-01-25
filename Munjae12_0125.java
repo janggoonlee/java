68bdd5eadc2d6cbca723d04865fea5b483b66081
janggoonlee <76644867+janggoonlee@users.noreply.github.com>
2021-01-25 20:36:54 +0900

    Add files via upload

---
+++ Munjae12_0125.java
@@ -0,0 +1,22 @@
+class Point{
+	int a, b;
+	
+	Point(int a, int b){
+		this.a=a;
+		this.b=b;
+	}
+	
+}
+
+
+public class Munjae12_0125 {
+
+	public static void main(String[] args) {
+
+		Point p = new Point(3,4);
+
+		
+		System.out.println(p);   //3.4출력 
+	}
+
+}
